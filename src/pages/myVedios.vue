b2860e32365a9ade166b192aac73dce9d59aab57
zhaohui <568792513@qq.com>
2018-04-14 22:52:39 +0800

    视频上传

--- src/pages/myVedios.vue
+++ src/pages/myVedios.vue
@@ -7,7 +7,7 @@
       </el-breadcrumb>
     </el-col>
 
-    <el-col :span="24" class="warp-main" >
+    <el-col :span="24" class="warp-main">
       <div style="margin: 30px 80px 30px 80px">
         <el-form label-width="80px">
           <fieldset>
@@ -30,19 +30,26 @@
                         <el-row :gutter="12" style="width: calc(100% - 16px)">
                           <el-col :span="24">
                             <el-form-item label="活动内容">
-                              <span style="color: darkseagreen;font-size: smaller" class="tt">{{ props.row.content }}</span>
+                              <span style="color: darkseagreen;font-size: smaller" class="tt">{{ props.row.content
+                                }}</span>
                             </el-form-item>
                           </el-col>
                         </el-row>
                         <el-row :gutter="12" style="width: calc(100% - 16px)">
                           <el-col :span="8">
-                            <el-button type="info" icon="el-icon-edit" round @click.native.prevent="handleRead(props.row)">详情</el-button>
+                            <el-button type="info" icon="el-icon-edit" round
+                                       @click.native.prevent="handleRead(props.row)">详情
+                            </el-button>
                           </el-col>
                           <el-col :span="8">
-                            <el-button type="info" icon="el-icon-success" round @click.native.prevent="handleApprove(props.row)">通过</el-button>
+                            <el-button type="info" icon="el-icon-success" round
+                                       @click.native.prevent="handleApprove(props.row)">通过
+                            </el-button>
                           </el-col>
                           <el-col :span="8">
-                            <el-button type="info" icon="el-icon-error" round @click.native.prevent="handleRefuse(props.row)">拒绝</el-button>
+                            <el-button type="info" icon="el-icon-error" round
+                                       @click.native.prevent="handleRefuse(props.row)">拒绝
+                            </el-button>
                           </el-col>
                         </el-row>
 
@@ -93,14 +100,14 @@
       </div>
     </el-col>
 
-    <el-col :span="24" class="warp-main" >
+    <el-col :span="24" class="warp-main">
       <div style="margin: 30px 80px 30px 80px">
         <el-form ref="form" :model="vedio" label-width="80px" :rules="rules">
           <fieldset>
             <legend>上传视频</legend>
             <el-row :gutter="12" style="width: calc(100% - 16px)">
               <el-col :span="10">
-                <el-form-item label="分类" prop="name">
+                <el-form-item label="分类" prop="category">
                   <el-tag v-for="category in categories"
                           size="medium"
                           closable
@@ -153,44 +160,54 @@
             </el-row>
             <el-row :gutter="12" style="width: calc(100% - 16px)">
               <el-col :span="12">
-                <el-form-item label="上传图片" prop="picture">
-                  <!--<el-upload-->
-                    <!--drag-->
-                    <!--class="video-img"-->
-                    <!--:file-list="ruleForm.videoImgList"-->
-                    <!--ref="uploadImg"-->
-                    <!--action=""-->
-                    <!--:show-file-list="true"-->
-                    <!--:auto-upload="false"-->
-                    <!--:limit="1"-->
-                    <!--:on-exceed="overLimit"-->
-                    <!--:before-upload="beforeVideoImgUpload">-->
-                    <!--<i class="el-icon-plus"></i>-->
-                    <!--<i class="avatar-uploader-icon"></i>-->
-                    <!--<div class="el-upload__tip">* 请点击此区域为选择一张图片作为视频封面</div>-->
-                    <!--<div class="el-upload__tip">* 上传图片只能是 JPG 格式!</div>-->
-                    <!--<div class="el-upload__tip">* 上传图片大小不能超过 2MB!</div>-->
-                  <!--</el-upload>-->
+                <el-form-item label="上传封面图片" prop="picture">
                   <el-upload
                     class="avatar-uploader"
-                    action=""
+                    ref="uploadImg"
+                    action="123"
+                    :limit="1"
                     :show-file-list="false"
                     :auto-upload="false"
+                    :on-change="onChangesImg"
                     :before-upload="beforeVideoImgUpload">
-                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
-                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                    <img v-if="showImg" :src="imageUrl" class="avatar">
+                    <div v-else class="icon-wrapper"><i class="el-icon-plus
+                    avatar-uploader-icon"></i></div>
                   </el-upload>
                 </el-form-item>
               </el-col>
             </el-row>
 
             <el-row :gutter="12" style="width: calc(100% - 16px)">
-              <form id="upload" enctype="multipart/form-data" method="post">
-                <input type="file" name="file" id="pic"/>
-                <input type="button" value="提交" onclick="uploadPic();"/>
-                <span class="showUrl"></span>
-                <img src="" class="showPic" alt="">
-              </form>
+              <el-col :span="12">
+                <el-form-item label="上传视频" prop="vedio">
+                  <el-upload
+                    class="avatar-uploader"
+                    ref="uploadVideo"
+                    action="123"
+                    :limit="1"
+                    :on-exceed="overLimit"
+                    :show-file-list="true"
+                    :file-list="vedio.videoFileList"
+                    :auto-upload="false"
+                    :on-change="onChangesVedio"
+                    :before-upload="beforeVideoUpload">
+                    <div class="icon-wrapper"><i class="el-icon-plus
+                    avatar-uploader-icon"></i></div>
+                  </el-upload>
+                </el-form-item>
+              </el-col>
+            </el-row>
+
+            <el-row :gutter="12" style="width: calc(100% - 16px)">
+              <!--<form id="upload" enctype="multipart/form-data" method="post">-->
+              <!--<input type="button" value="提交" onclick="uploadPic();"/>-->
+              <!--<span class="showUrl"></span>-->
+              <!--<img src="" class="showPic" alt="">-->
+              <!--</form>-->
+              <el-form-item>
+                <el-button type="primary" v-on:click="submitForm('vedio')">立即发布</el-button>
+              </el-form-item>
             </el-row>
           </fieldset>
         </el-form>
@@ -204,83 +221,94 @@
   import Fetch from './../api/index'
   import _ from 'lodash';
   import Rule from './../common/lib/vedio-rule'
+
   export default {
-    data(){
-      const vm =this;
-      return{
-        vedio:{},
-        vediosData:[],
-        tableData:[],
-        splitTableData:[],
-        copyTableData:[],
-        pageSize:2,
-        currenyPage:1,
-        categories:[],
-        types:[],
-        newType:{},
-        rules:Rule.call(vm, 'form'),
-        ruleForm: {
-          name: '',
-          type: '',
-          desc: '',
+    data() {
+      const vm = this;
+      return {
+        vedio: {
           videoImgList: [],
-          //videoFileList: []
+          videoFileList: []
         },
-        imageUrl:'',
+        vediosData: [],
+        tableData: [],
+        splitTableData: [],
+        copyTableData: [],
+        pageSize: 2,
+        currenyPage: 1,
+        categories: [],
+        types: [],
+        newType: {},
+        rules: Rule.call(vm, 'form'),
+        imgFile: {},
+        videoFile: {},
+        showImg: false,
+        imageUrl: '',
+        uploadForm: new FormData(),
+
       }
     },
-    methods:{
-      overLimit() {
-        let sel = this;
-        sel.$message.error('上传数量为1');
-      },
-      beforeVideoImgUpload(file) {
+    methods: {
+      onChangesImg(file, fileList) {
         console.log(file);
-        let sel = this;
-        this.imgFile = file;
         let isLt2M = file.size / 1024 / 1024 < 1;
         if (!isLt2M) {
-          sel.$message.error('上传视频封面图片大小不能超过 1MB!');
+          this.$message.error('上传视频封面图片大小不能超过 1MB!');
           return false;
         }
-        this.uploadForm.append('imgFile', file);
         this.imageUrl = URL.createObjectURL(file.raw);
+        this.showImg = true;
         return isLt2M;
       },
+      onChangesVedio(file, fileList) {
+        console.log(file);
+        let isLt2M = file.size / 1024 / 1024 < 1000;
+        if (!isLt2M) {
+          this.$message.error('上传视频大小不能超过 1GB!');
+          return false;
+        }
+        return isLt2M;
+      },
+      overLimit() {
+        let sel = this;
+        sel.$message.error('仅能上传一个文件');
+      },
+      beforeVideoImgUpload(file) {
+        this.imgFile = file;
+        this.uploadForm.append('imgFile', file);
+      },
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      beforeVideoUpload(file) {
+        this.videoFile = file;
+        this.uploadForm.append('videoFile', file);
+      },
+      async submitForm(formName) {
+        let sel = this;
+        this.uploadForm.append('name', this.vedio.name);
+        this.uploadForm.append('categories', this.categories);
+        this.uploadForm.append('summary', this.vedio.summary);
+        this.$refs.uploadImg.submit();  // 提交时触发了before-upload函数
+        this.$refs.uploadVideo.submit();   // 提交时触发了before-upload函数
+        let fetch = await Fetch.getFetch();
+        let result = fetch.post('/api/vedio/addVideo', this.uploadForm);
+      },
 
 
       handleSizeChange(val) {
-        this.splitTableData =_.chunk(this.tableData,val)||[];
-        this.copyTableData =this.splitTableData[this.currenyPage-1];
+        this.splitTableData = _.chunk(this.tableData, val) || [];
+        this.copyTableData = this.splitTableData[this.currenyPage - 1];
       },
       handleCurrentChange(val) {
-        this.currenyPage =val;
-        this.copyTableData =this.splitTableData[this.currenyPage-1];
+        this.currenyPage = val;
+        this.copyTableData = this.splitTableData[this.currenyPage - 1];
       },
-      handleCategoryClose(val){
+      handleCategoryClose(val) {
         console.log("jin")
-          console.log("val",val);
+        console.log("val", val);
 //        _.remove((this.categories||[]),category=>{
 //          return category.id == val.id;
 //        })
-        this.categories = (this.categories||[]).filter(category=>{
+        this.categories = (this.categories || []).filter(category => {
           return category.id != val.id;
         })
       },
@@ -293,19 +321,19 @@
       handleAddType() {
         if (this.newType) {
           var temp = {};
-          (this.types||[]).forEach(type=>{
-            if(type.id == this.newType){
+          (this.types || []).forEach(type => {
+            if (type.id == this.newType) {
               temp = type;
             }
           })
-          console.log("copy",temp)
+          console.log("copy", temp)
           this.categories.push(temp);
           this.newType = {};
         }
       },
 
-      async loadTypeList(){
-        let fetch  = Fetch.getFetch();
+      async loadTypeList() {
+        let fetch = Fetch.getFetch();
         let result = await fetch.get('/api/type/list');
         this.types = result.data;
       },
@@ -318,7 +346,41 @@
       },
 
     },
-    mounted(){
+    mounted() {
     },
   }
 </script>
+
+<style scoped="">
+  .avatar-uploader .el-upload {
+    border: 1px dashed #b0d9d5;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #9d9788;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+
+  .avatar {
+    width: 360px;
+    height: 180px;
+    display: block;
+  }
+
+  .icon-wrapper {
+    border: 1px dashed #409EFF;
+    border-radius: 6px;
+  }
+</style>
